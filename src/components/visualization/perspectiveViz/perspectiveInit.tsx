fde1e0d5c25770296699d8494652d0f230968f99
Eileen Li <eileen@dragonscale.ai>
2025-07-07 18:03:45 -0700

    fix: update perspectiveInit

--- src/components/visualization/perspectiveViz/perspectiveInit.tsx
+++ src/components/visualization/perspectiveViz/perspectiveInit.tsx
@@ -1,20 +1,29 @@
 import type { Client } from '@finos/perspective'
-import * as perspective from '@finos/perspective'
+import perspective from '@finos/perspective'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as SERVER_WASM from '@finos/perspective/dist/wasm/perspective-server.wasm'
 import perspective_viewer from '@finos/perspective-viewer'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as CLIENT_WASM from '@finos/perspective-viewer/dist/wasm/perspective-viewer.wasm'
 
+let initPromise: Promise<void>
 export let globalPerspectiveWorker: Client
 
-const perspectiveInitPromise = Promise.all([
-  perspective.init_server(SERVER_WASM),
-  perspective_viewer.init_client(CLIENT_WASM as any),
-])
+export function initPerspective() {
+  if (initPromise) {
+    return initPromise
+  }
 
-export function initPerspective(): void {
-  perspectiveInitPromise.then(() =>
-    perspective.worker().then((worker: Client) => {
+  initPromise = Promise.all([
+    perspective.init_server(SERVER_WASM),
+    perspective_viewer.init_client(CLIENT_WASM as any),
+  ])
+    .then(() => perspective.worker())
+    .then((worker: Client) => {
       globalPerspectiveWorker = worker
     })
-  )
+
+  return initPromise
 }
